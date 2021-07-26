7af071c8f453fa602c23e35368fa34efd12ce050
barisbicer <baris_bicer@hotmail.com>
2021-07-26 12:16:06 +0300

    first commit

---
+++ src/test/java/com/heas/azure/demo/SpringAzureDemoHeas1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heas.azure.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemoHeas1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
